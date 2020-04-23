63e27896d1dc59b485d41f10f34a72d7d60279a0
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-04-23 07:41:37 -0700

    Fixed broken links in side Navbar

--- portfolio_website/src/components/Navbar.js
+++ portfolio_website/src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Navitem from './Navitem';
+
+
 class Navbar extends Component {
 constructor(props)
 {
@@ -17,18 +19,18 @@ this.setState({'NavItemId':x},()=>{
 document.getElementById(this.state.NavItemActive).classList.add('active');
 });
 };
-render() {
-return (
-<nav>
-<ul>
-<Navitem item="Home" tolink="/"  activec={this.activeitem}></Navitem>
-<Navitem item="About" tolink="/about"  activec={this.activeitem}></Navitem>
-<Navitem item="Education" tolink="/education"  activec={this.activeitem}></Navitem>
-<Navitem item="Skills" tolink="/skills"  activec={this.activeitem}></Navitem>
-<Navitem item="Contact" tolink="/contact"  activec={this.activeitem}></Navitem>
-</ul>
-</nav>
-)
-}
+	render() {
+		return (
+			<nav>
+			<ul>
+			<Navitem item="Home" tolink="/"  activec={this.activeitem}></Navitem>
+			<Navitem item="About" tolink="/about"  activec={this.activeitem}></Navitem>
+			<Navitem item="Education" tolink="/education"  activec={this.activeitem}></Navitem>
+			<Navitem item="Skills" tolink="/skills"  activec={this.activeitem}></Navitem>
+			<Navitem item="Contact" tolink="/contact"  activec={this.activeitem}></Navitem>
+			</ul>
+			</nav>
+			)
+	}
 }
 export default Navbar
--- portfolio_website/src/components/Navitem.js
+++ portfolio_website/src/components/Navitem.js
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 class Navitem extends Component {
 render() {
 return (
-<li id={this.props.item}>
-<Link to={this.props.tolink} onClick={this.props.activec.bind(this,this.props.item)}>{this.props.item}</Link>
+<li>
+<Link to={this.props.tolink} >
+{this.props.item}
+</Link>
 </li>
 )
 }
