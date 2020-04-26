7a9174ab586e82534fc365ec95f82a9a021ea07e
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-04-26 16:13:49 -0700

    Changed profile picture

--- portfolio_website/src/contents/Home.js
+++ portfolio_website/src/contents/Home.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import profilepic from '../img/download.png';
+import profilepic from '../img/milo_profile.jpg';
 import Social from '../components/Social'
+
 class Home extends Component {
-render() {
-return (
-<div className="condiv home">
-<img src={profilepic} className="profilepic"></img>
-<Social />
-</div>
-)
-}
+	render() {
+		return (
+		<div className="condiv home">
+		<img src={profilepic} className="profilepic" width="500" height="600"></img>
+			<Social />
+		</div>
+	)
+	}
 }
 export default Home
