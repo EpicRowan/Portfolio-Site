0fda0d9c652cdadb8654b0b1723410e8af9514d2
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-07-06 07:25:46 -0700

    Added header and cleaned up unnecessary css code

--- portfolio_website/src/contents/Home.js
+++ portfolio_website/src/contents/Home.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import profilepic from '../img/milo_profile.jpg';
 import Services from '../components/Services'
+import Header from '../components/Header'
 
 class Home extends Component {
 	render() {
 		return (
 
 		<div className="home">
-					<h1> Hi, I am Bob </h1>
+		<Header />
+			<h1> Hi, I am Bob </h1>
 		<img src={profilepic} className="profilepic"></img>
 		<Services />
 		</div>
