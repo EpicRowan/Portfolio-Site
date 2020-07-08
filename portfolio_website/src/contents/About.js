fea391bd2beb4a0df745c2a6ee3dbbc9a6fd0ea4
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-07-07 21:02:17 -0700

    Succesfully added Projects button

--- portfolio_website/src/contents/About.js
+++ portfolio_website/src/contents/About.js
@@ -12,17 +12,15 @@ class About extends Component {
 
 			<p className="about_section">
 			<h3> Title 1</h3>
-				I do this
-			
+				What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?
 			
 			<h3> Title 2</h3>
 				This too
 			
-			
 			<h3> Title 3</h3>
 				Also this
 			</p>
-
+			<button className="btn"> My Projects</button>
 		</div>
 		)
 	}
